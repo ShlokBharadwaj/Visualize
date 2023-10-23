efa3e9e7938a03e5e349deacd137df3434f713dc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-23 16:35:41 +0530

    Update: Handle Updates, Additions and deletions in MongoDB using 'title' as unique identifier

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const apiRoutes = require('./routes/api');
 const fs = require('fs');
 const { MongoClient } = require('mongodb');
+const crypto = require('crypto');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +13,12 @@ const dbName = process.env.DB_NAME;
 const collectionName = process.env.COLLECTION_NAME;
 let jsonData = [];
 
-app.use('/api', apiRoutes);
+// Function to generate a hash value for the JSON data
+const generateHash = (data) => {
+  return crypto.createHash('sha1').update(JSON.stringify(data)).digest('hex');
+}
+
+let previousHash = null;
 
 // Function to read data from the JSON file
 const readData = () => {
@@ -24,10 +30,14 @@ const readData = () => {
       } else {
         try {
           const currentData = JSON.parse(data);
-          if (JSON.stringify(jsonData) !== JSON.stringify(currentData)) {
+          const currentHash = generateHash(currentData);
+
+          if (previousHash !== currentHash) {
             jsonData = currentData;
             updateDatabase();
-          } else {
+            previousHash = currentHash;
+          }
+          else {
             console.log('No modifications in JSON file');
           }
         } catch (parseErr) {
@@ -42,19 +52,46 @@ const readData = () => {
 
 // Function to update the database with the modified JSON data
 const updateDatabase = async () => {
+  let client;
   try {
-    const client = await MongoClient.connect(url);
+    client = await MongoClient.connect(url);
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    await collection.deleteMany({});
+    // Keep track of existing titles in the database
+    const existingTitles = new Set();
 
-    const result = await collection.insertMany(jsonData);
-    console.log(`${result.insertedCount} documents inserted successfully`);
+    // Fetch existing titles from the database
+    const existingData = await collection.find({}).toArray();
+    existingData.forEach((data) => {
+      existingTitles.add(data.title);
+    });
 
-    client.close();
+    for (let i = 0; i < jsonData.length; i++) {
+      const currentData = jsonData[i];
+      if (existingTitles.has(currentData.title)) {
+        // If data with the same title exists, update the document
+        await collection.updateOne({ title: currentData.title }, { $set: currentData });
+        // Remove the title from the set
+        existingTitles.delete(currentData.title);
+      } else {
+        // If data with the same title does not exist, insert a new document
+        await collection.insertOne(currentData);
+      }
+    }
+
+    // Delete documents that no longer exists in the JSON file
+    for (let title of existingTitles) {
+      await collection.deleteOne({ title: title });
+    }
+
+    console.log('Database updated successfully');
   } catch (err) {
     console.error('Error connecting to the database', err);
+  } finally {
+    if (client) {
+      client.close();
+    }
   }
 };
 
@@ -74,6 +111,8 @@ fs.watchFile('./data/jsondata.json', { persistent: true, interval: 500 }, (curr,
     console.error('Error watching file:', watchErr);
   }
 });
+
+app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
   res.send(`Welcome to the Data Visualization Dashboard API<br>You can access the data at <a href="/api/data">/api/data</a>`);
