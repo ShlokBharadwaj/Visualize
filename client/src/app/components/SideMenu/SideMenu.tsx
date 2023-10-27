7e32aa92a97c321b8bd7dbd1afcb5e487db40138
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-27 13:08:09 +0530

    Add: Intial SideMenu

--- client/src/app/components/SideMenu/SideMenu.tsx
+++ client/src/app/components/SideMenu/SideMenu.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BarChart, Settings, AccountBox } from '@mui/icons-material';
 
 const SideMenu = () => {
     return (
-        <ul className="bg-transparent-100 p-4 list-disc">
-            <li className="p-2">Analytics</li>
-            <li className="p-2">Profile</li>
-            <li className="p-2">Settings</li>
-        </ul>
-    )
+        <div className="bg-white w-64 h-screen flex flex-col">
+            <div className="p-4">
+                <h2 className="text-xl font-bold text-gray-800">Menu</h2>
+            </div>
+            <ul className="flex-grow flex flex-col justify-start p-4">
+                <li className="p-4 flex items-center">
+                    <BarChart className="text-gray-400 mr-2" />
+                    <span className="text-gray-800">Analytics</span>
+                </li>
+                <li className="p-4 flex items-center">
+                    <AccountBox className="text-gray-400 mr-2" />
+                    <span className="text-gray-800">Profile</span>
+                </li>
+                <li className="p-4 flex items-center">
+                    <Settings className="text-gray-400 mr-2" />
+                    <span className="text-gray-800">Settings</span>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default SideMenu;
