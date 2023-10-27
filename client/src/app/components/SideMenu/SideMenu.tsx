abfdb30bc59ffc037bf8029c09678d7e5fb9cea3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-27 18:54:59 +0530

    Add: Navigation to Dashboard elements

--- client/src/app/components/SideMenu/SideMenu.tsx
+++ client/src/app/components/SideMenu/SideMenu.tsx
@@ -12,9 +12,11 @@ const SideMenu = () => {
             <div className="p-4 flex items-center justify-between">
                 <div className='flex items-center'>
                     {!isCollapsed && (
-                        <div className="mr-2">
-                            <Image src="/assets/Visualize-logos_transparent.png" alt="Visualize logo" width={160} height={40} layout="responsive" className="text-xl font-bold text-gray-800" />
-                        </div>
+                        <a href="/">
+                            <div className="mr-2">
+                                <Image src="/assets/Visualize-logos_transparent.png" alt="Visualize logo" width={160} height={40} layout="responsive" onClick={() => setIsCollapsed(!isCollapsed)} className={`text-xl font-bold text-gray-800 ${isCollapsed ? 'block' : 'block'}`} />
+                            </div>
+                        </a>
                     )}
                     <button className="p-1 focus:outline-none text-gray-400 hover:bg-gray-200 rounded-md" onClick={() => setIsCollapsed(!isCollapsed)}>
                         {isCollapsed ? '>>' : '<<'}
@@ -22,18 +24,24 @@ const SideMenu = () => {
                 </div>
             </div>
             <ul className="flex-grow flex flex-col justify-start p-4">
-                <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
-                    <BarChart className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
-                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Analytics</span>
-                </li>
-                <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
-                    <AccountBox className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
-                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Profile</span>
-                </li>
-                <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
-                    <Settings className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
-                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Settings</span>
-                </li>
+                <a href="/dashboard/analytics">
+                    <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
+                        <BarChart className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
+                        <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Analytics</span>
+                    </li>
+                </a>
+                <a href="/dashboard/profile">
+                    <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
+                        <AccountBox className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
+                        <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Profile</span>
+                    </li>
+                </a>
+                <a href="/dashboard/settings">
+                    <li className="p-1 flex items-center cursor-pointer my-2 hover:bg-gray-200 rounded-md">
+                        <Settings className={`text-gray-400 mr-2 ${isCollapsed ? 'block' : 'block'}`} />
+                        <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Settings</span>
+                    </li>
+                </a>
             </ul>
         </div>
     );
