5af1e66443b87a27619062976b1c5412ad08f1f1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-27 18:27:02 +0530

    Update: Adjust Header component styles

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
           placeholder="Search"
           className="bg-transparent text-gray-800 rounded-md py-3 px-4 w-full border outline-none pl-10"
         />
-        <AccountCircleOutlined className="absolute right-6 top-7 text-gray-400" />
-        <NotificationsOutlined className="absolute right-14 top-7 text-gray-400" />
-        <LightModeOutlined className="absolute right-[5.5rem] top-7 text-gray-400" />
+        <AccountCircleOutlined className="absolute right-6 top-7 text-gray-400 rounded-md hover:bg-gray-200" />
+        <NotificationsOutlined className="absolute right-14 top-7 text-gray-400 rounded-md hover:bg-gray-200" />
+        <LightModeOutlined className="absolute right-[5.5rem] top-7 text-gray-400 rounded-md hover:bg-gray-200" />
       </div>
     </header>
   );
