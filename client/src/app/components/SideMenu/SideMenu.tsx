41afe3b0fdbb33112902e01efa96f6992e0864f4
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-27 16:21:21 +0530

    Add: Logo to Sidemenu

--- client/src/app/components/SideMenu/SideMenu.tsx
+++ client/src/app/components/SideMenu/SideMenu.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { BarChart, Settings, AccountBox } from '@mui/icons-material';
+import Image from 'next/image';
 
 const SideMenu = () => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
     return (
-        <div className="bg-white w-64 h-screen flex flex-col">
-            <div className="p-4">
-                <h2 className="text-xl font-bold text-gray-800">Menu</h2>
+        <div className={`bg-white w-64 h-screen flex flex-col ${isCollapsed ? 'w-16' : 'w-64'}`}>
+            <div className="p-4 flex items-center justify-between">
+                <div className='flex items-center'>
+                    {!isCollapsed && (
+                        <div className="mr-2">
+                            <Image src="/assets/Visualize-logos_transparent.png" alt="Visualize logo" width={160} height={40} layout="responsive" className="text-xl font-bold text-gray-800" />
+                        </div>
+                    )}
+                    <button className="p-1 focus:outline-none text-black" onClick={() => setIsCollapsed(!isCollapsed)}>
+                        {isCollapsed ? '>>' : '<<'}
+                    </button>
+                </div>
             </div>
             <ul className="flex-grow flex flex-col justify-start p-4">
                 <li className="p-4 flex items-center">
-                    <BarChart className="text-gray-400 mr-2" />
-                    <span className="text-gray-800">Analytics</span>
+                    <BarChart className={`text-gray-400 mr-2 ${isCollapsed ? 'hidden' : 'block'}`} />
+                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Analytics</span>
                 </li>
                 <li className="p-4 flex items-center">
-                    <AccountBox className="text-gray-400 mr-2" />
-                    <span className="text-gray-800">Profile</span>
+                    <AccountBox className={`text-gray-400 mr-2 ${isCollapsed ? 'hidden' : 'block'}`} />
+                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Profile</span>
                 </li>
                 <li className="p-4 flex items-center">
-                    <Settings className="text-gray-400 mr-2" />
-                    <span className="text-gray-800">Settings</span>
+                    <Settings className={`text-gray-400 mr-2 ${isCollapsed ? 'hidden' : 'block'}`} />
+                    <span className={`text-gray-800 ${isCollapsed ? 'hidden' : 'block'}`}>Settings</span>
                 </li>
             </ul>
         </div>
