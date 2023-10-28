f155633d54e0e5a0f58e1c820f4f0396d871a793
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-28 13:03:25 +0530

    Update: Interface to handle mixed data types

--- client/src/app/dashboard/Dashboard.tsx
+++ client/src/app/dashboard/Dashboard.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+'use client'
 
-const Dashboard = () => {
-    return (
-        <h1 className="text-3xl font-bold text-center mt-8">Dashboard</h1>
-    )
+import React, { useState } from 'react';
+
+interface IData {
+  end_year: string | number;
+  intensity: number;
+  sector: string;
+  topic: string;
+  insight: string;
+  url: string;
+  region: string;
+  start_year: string | number;
+  impact: number | number;
+  added: string;
+  published: string;
+  country: string;
+  relevance: number;
+  pestle: string;
+  source: string;
+  title: string;
+  likelihood: number;
 }
+
+const Dashboard: React.FC = () => {
+  const [data, setData] = useState<IData[]>([]);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-8">Dashboard</h1>
+    </div>
+  );
+};
 
 export default Dashboard;
