5e5bdd41208af7d5f0b3608979771861868f607d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-28 21:24:51 +0530

    Add: Time series line chart using d3

--- client/src/app/dashboard/Dashboard.tsx
+++ client/src/app/dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import * as d3 from 'd3';
 
 interface IData {
   end_year: string | number;
@@ -25,10 +26,17 @@ interface IData {
 
 const Dashboard: React.FC = () => {
   const [data, setData] = useState<IData[]>([]);
+  const chartRef = useRef(null);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (data.length) {
+      createLineChart(data);
+    }
+  }, [data]);
 
   const fetchData = async () => {
     try {
@@ -39,9 +47,29 @@ const Dashboard: React.FC = () => {
     }
   };
 
+  const createLineChart = (data: IData[]) => {
+    const svg = d3.select(chartRef.current)
+      .append('svg')
+      .attr('width', 400)
+      .attr('height', 200);
+
+    const line = d3.line<IData>()
+      .x((d, i) => i * 40)
+      .y((d) => 200 - 10 * (d.intensity as number));
+
+    svg.append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('d', line);
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center mt-8">Dashboard</h1>
+    <div className="flex flex-col h-screen bg-gray-100">
+      <header className="bg-white text-gray-800 shadow-md p-4">
+        <h1 className="text-3xl font-bold text-center">Dashboard</h1>
+      </header>
+      <div ref={chartRef} id="lineChart" className="mt-8 mx-4"></div>
     </div>
   );
 };
