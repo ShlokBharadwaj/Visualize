2e68c6de1905110cc93ed1187ea49a2b42678bca
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-29 12:51:18 +0530

    Add: Line chart to Main Data Visualization div

--- client/src/app/dashboard/Dashboard.tsx
+++ client/src/app/dashboard/Dashboard.tsx
@@ -78,7 +78,7 @@ const Dashboard: React.FC = () => {
         <div className="w-full overflow-y-auto p-4">
           {/* Main Content */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-red-300 md:p-60 p-28 rounded-md">Main Data Visualization</div>
+            <div ref={chartRef} className="bg-red-300 md:p-60 p-28 rounded-md">Main Data Visualization</div>
             <div className="bg-green-300 md:p-60 p-28 rounded-md">Data Visualization 1</div>
             <div className="bg-yellow-300 p-10 rounded-md">Data Visualization 2</div>
             <div className="bg-purple-300 p-10 rounded-md">Data Visualization 3</div>
