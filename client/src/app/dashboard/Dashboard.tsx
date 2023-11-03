5b52cc341067b9f009bffc31307c06afc0a6c4d8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-03 13:08:23 +0530

    Add: Bar chart for Intensity data

--- client/src/app/dashboard/Dashboard.tsx
+++ client/src/app/dashboard/Dashboard.tsx
@@ -28,10 +28,17 @@ interface IData {
 
 const Dashboard: React.FC = () => {
   const [data, setData] = useState<IData[]>([]);
+  const chartRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (data.length) {
+      createBarChart(data);
+    }
+  }, [data]);
 
   const fetchData = async () => {
     try {
@@ -40,6 +47,54 @@ const Dashboard: React.FC = () => {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+  };
+
+  const createBarChart = (data: IData[]) => {
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = 400 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3
+      .select(chartRef.current)
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.intensity.toString()))
+      .range([0, width])
+      .padding(0.1);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.intensity) || 0])
+      .range([height, 0]);
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', d => x(d.intensity.toString()) || 0)
+      .attr('y', d => y(d.intensity))
+      .attr('width', x.bandwidth())
+      .attr('height', d => height - y(d.intensity))
+      .attr('fill', 'steelblue');
+
+    svg.selectAll('text')
+      .data(data)
+      .enter()
+      .append('text')
+      .text(d => d.intensity)
+      .attr('x', d => x(d.intensity.toString()) || 0)
+      .attr('y', d => y(d.intensity) - 4)
+      .attr('font-size', '10px');
+
+    svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
+
+    svg.append('g').call(d3.axisLeft(y));
   };
 
   return (
@@ -51,7 +106,7 @@ const Dashboard: React.FC = () => {
         </div>
         <div className="w-full overflow-y-auto p-4">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-red-300 md:p-10 p-4 rounded-md">Main Data Visualization</div>
+            <div ref={chartRef} className="bg-red-300 md:p-10 p-4 rounded-md">Main Data Visualization</div>
             <div className="bg-green-300 md:p-60 p-28 rounded-md">Data Visualization 1</div>
             <div className="bg-yellow-300 p-10 rounded-md">Data Visualization 2</div>
             <div className="bg-purple-300 p-10 rounded-md">Data Visualization 3</div>
